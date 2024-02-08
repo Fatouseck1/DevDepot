367d5033e56e20436a8708952920c2d07f15fb01
Fatou <fafaseck705@gmail.com>
2024-02-08 15:00:13 +0100

    Affichage de l'année de naissance de l'utilisateur

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-first_name = input("Entrez votre prénom : ")
-last_name = input("Entrez votre nom : ")
-print("Bonjour", first_name, last_name)
+from datetime import datetime
+
+current_year = datetime.now().year
+birth_year = current_year - int(age)
+print("Vous êtes né(e) en", birth_year)
